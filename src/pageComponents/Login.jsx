7b2732c104cfa29635914c66824e9eb4a52047d8
Shivam Dubey <shivamdubey1081@gmail.com>
2026-06-04 17:28:22 +0530

    feature: Implemented the Signup Page.

--- src/pageComponents/Login.jsx
+++ src/pageComponents/Login.jsx
@@ -6,9 +6,12 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("aditya@gmail.com");
-  const [password, setPassword] = useState("Aditya@1801");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [error, setError] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -33,8 +36,31 @@ const Login = () => {
     }
   };
 
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        {
+          emailId,
+          password,
+          firstName,
+          lastName,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      dispatch(addUser(res?.data?.data));
+      navigate("/profile");
+      return res;
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+      console.log(err);
+    }
+  };
+
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && isLoginForm) {
       handleLogin();
     }
   };
@@ -43,7 +69,9 @@ const Login = () => {
     <div className="flex justify-center items-center my-10">
       <div className="card card-border bg-base-300 w-96 shadow-xlßß">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Signup"}
+          </h2>
           <div>
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Email Id</legend>
@@ -68,11 +96,50 @@ const Login = () => {
               />
             </fieldset>
           </div>
+
+          {!isLoginForm && (
+            <div>
+              <div>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Firstname</legend>
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    className="input"
+                  />
+                </fieldset>
+              </div>
+
+              <div>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Lastname</legend>
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    className="input"
+                  />
+                </fieldset>
+              </div>
+            </div>
+          )}
+
           <p className="text-error">{error}</p>
-          <div className="card-actions justify-center">
-            <button onClick={handleLogin} className="btn btn-primary my-2">
-              Login
+          <div className="flex flex-col card-actions items-center">
+            <button
+              onClick={isLoginForm ? handleLogin : handleSignup}
+              className="btn btn-primary my-2 w-2/3"
+            >
+              {isLoginForm ? "Login" : "Signup"}
             </button>
+            <p className="link" onClick={() => setIsLoginForm(!isLoginForm)}>
+              {isLoginForm
+                ? "Don't have an account? Signup Here"
+                : "Already have an account? Login Here"}
+            </p>
           </div>
         </div>
       </div>
